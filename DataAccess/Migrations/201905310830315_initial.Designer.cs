c04d30e60534ec609ea03dbcc78789d1c1a8b03f
Hisyam6706154061 <hisyam.lateaf@gmail.com>
2019-05-31 15:34:13 +0700

    -ubah connection string -apus migrasi -add-migration initial -update-database

--- DataAccess/Migrations/201905310830315_initial.Designer.cs
+++ DataAccess/Migrations/201905310830315_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace DataAccess.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class addingModelSupplierItem : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(addingModelSupplierItem));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201905280838029_addingModelSupplierItem"; }
+            get { return "201905310830315_initial"; }
         }
         
         string IMigrationMetadata.Source
---
+++ DataAccess/Migrations/201905310830315_initial.cs
@@ -0,0 +1,26 @@
+namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.TB_M_Item", "Supplier_Id", "dbo.TB_M_Supplier");
+            DropIndex("dbo.TB_M_Item", new[] { "Supplier_Id" });
+            AlterColumn("dbo.TB_M_Item", "Supplier_Id", c => c.Int());
+            CreateIndex("dbo.TB_M_Item", "Supplier_Id");
+            AddForeignKey("dbo.TB_M_Item", "Supplier_Id", "dbo.TB_M_Supplier", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TB_M_Item", "Supplier_Id", "dbo.TB_M_Supplier");
+            DropIndex("dbo.TB_M_Item", new[] { "Supplier_Id" });
+            AlterColumn("dbo.TB_M_Item", "Supplier_Id", c => c.Int(nullable: false));
+            CreateIndex("dbo.TB_M_Item", "Supplier_Id");
+            AddForeignKey("dbo.TB_M_Item", "Supplier_Id", "dbo.TB_M_Supplier", "Id", cascadeDelete: true);
+        }
+    }
+}
